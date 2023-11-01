78219b600c11dc5a06d674760e99b1db80c7ba3b
lukasilverio94 <lukasilverio94@gmail.com>
2023-11-01 16:26:22 +0100

    added files and done till exercise 3

---
+++ script.js
@@ -0,0 +1,71 @@
+//Exercises Part 3-
+
+// Exercise 1
+// The following function returns true if the parameter age is greater than 18.
+// function checkAge(age) {
+//     if (age > 18) {
+//       return true;
+//     } else {
+//       return confirm('Do you have your parents permission to access this page?');
+//     }
+// }
+// Otherwise, it asks for a confirmation and returns its result.
+// Rewrite it, to perform the same, but without if...else, and using arrow function.
+//Solution:
+
+const checkAge = (age) => {
+  return age > 18
+    ? true
+    : confirm("Do you have your parents permission to access this page?");
+};
+let result = checkAge(19);
+console.log(result);
+
+// ###################################################################################################
+
+//Exercise 2
+// Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
+const pow = (x, n) => {
+  if (n === 0) {
+    return 1;
+  } else if (n < 0) {
+    return 1 / (x * pow(x, -n - 1)); //case negative numbers
+  } else {
+    return x * pow(x, n - 1);
+  }
+};
+console.log(pow(3, 2));
+// JAVASCRIPT
+// pow(3, 2) = 3 * 3 = 9
+// pow(3, 3) = 3 * 3 * 3 = 27
+// pow(1, 100) = 1 * 1 * ...* 1 = 1
+// P.S. In this task the function should support only natural values of n: integers up from 1.
+
+// ########################################################################################################
+//3. Exercise
+// Replace Function Expressions with arrow functions in the code:
+// JAVASCRIPT
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes()
+//   else no()
+// }
+// ask(
+//   "Do you agree?",
+//   function() { alert("You agreed.") },
+//   function() { alert("You canceled the execution.") }
+// )
+
+//Solution:
+const ask = (question, yes, no) => {
+  return confirm(question) ? yes() : no();
+};
+
+ask(
+  "Do you agree?",
+  () => {
+    alert("You agreed.");
+  },
+  () => {
+    alert("You canceled the execution.");
+  }
+);
